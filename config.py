de75edb9a196f07967ec5f4a2953e0341a60550e
Mekala Siva Prasad <253207859+SivaPrasad110@users.noreply.github.com>
2026-07-05 16:29:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,15 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# ==========================================
-# GEMINI SETTINGS
-# ==========================================
+# =========================================
+# xAI API
+# =========================================
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+XAI_API_KEY = os.getenv("XAI_API_KEY")
 
-# Recommended model
-GEMINI_MODEL = "gemini-2.0-flash"
-
-# Alternative:
-# GEMINI_MODEL = "gemini-1.5-flash"
-print("API Key:", GEMINI_API_KEY[:10])
+# Grok Model
+XAI_MODEL = "grok-4"
